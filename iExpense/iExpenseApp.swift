85d4883e13cbbad2d73036a539ab6074113e5434
iiiivaska <iiiivaska@yandex.ru>
2021-10-27 21:00:09 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Василий Буланов on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
