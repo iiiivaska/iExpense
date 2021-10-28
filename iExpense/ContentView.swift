28706b22fce46c750f5cbb8662fd5d585419b7d2
iiiivaska <iiiivaska@yandex.ru>
2021-10-28 16:08:26 +0300

    Create list of expenses with test button.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,25 +7,43 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
-    
-    struct User: Codable {
-        var firstName: String
-        var lastName: String
-    }
-    
-    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+    @ObservedObject var expense = Expenses()
     
     var body: some View {
-        Button("Save User") {
-            let encoder = JSONEncoder()
-            
-            if let data = try? encoder.encode(self.user) {
-                UserDefaults.standard.set(data, forKey: "UserDate")
+        NavigationView {
+            List {
+                ForEach(expense.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
             }
+            .navigationTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    self.expense.items.append(expense)
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
         }
     }
     
+    func removeItems(at offsets: IndexSet) {
+        expense.items.remove(atOffsets: offsets)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
